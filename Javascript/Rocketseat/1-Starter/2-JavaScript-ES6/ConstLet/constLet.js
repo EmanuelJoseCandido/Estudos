4f3a4ff12fdbef10679a417741cf5591c344360b
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2020-07-05 00:00:15 +0100

    Add const and let.

---
+++ Javascript/Rocketseat/1-Starter/2-JavaScript-ES6/ConstLet/constLet.js
@@ -0,0 +1,18 @@
+/* const user = {
+    name: "Emanuel"
+};
+
+user.name = "Aldair";
+
+console.log(user); */
+
+function test(x) {
+    let y = 2;
+    
+    if(x > 5){
+        let y = 4;
+        console.log(x, y);
+    }
+}
+
+console.log(test(10));
